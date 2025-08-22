c2786a05638009561657e6b345774bdbb3ea4698
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2025-08-22 14:58:59 -0600

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,23 @@
+def ask_questions(name):
+    print(f"Hello, {name}!")
+    question1 = "What is your favorite color? "
+    answer1 = input(question1)
+    question2 = "What is your favorite food? "
+    answer2 = input(question2)
+    question3 = "What city do you live in? "
+    answer3 = input(question3)
+    print(f"Your favorite color is {answer1}.")
+    print(f"Your favorite food is {answer2}.")
+    print(f"You live in {answer3}.")
+    # Ask for X or O and respond accordingly
+    XO = input("Choose X  OR O: ").strip().upper()
+
+    if XO == "X":
+        print("You wrote X!")
+    elif XO == "O":
+        print("You wrote O!")
+    else:
+        print("Invalid choice. Please choose X or O next time.")
+
+name = "Javier"
+ask_questions(name)
