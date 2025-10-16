a2e10440bf4971d66e250192cfac5efd17e8fa8f
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2025-10-15 20:43:37 -0600

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,14 @@
+name = input("Enter your name")
+
+print(f"Hello, {name}!")
+
+
+
+
+def explain_for_loop():
+    for i in range(5):
+        print("Learning Python!")
+    arr = ["apple", "banana", "cherry"]
+    for i in arr :
+        print(i)
+explain_for_loop()
