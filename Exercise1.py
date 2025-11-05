387036060ca533478e73e741aae84a9c6e84d90f
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2025-11-04 22:04:17 -0600

    Add files via upload

--- Exercise1.py
+++ Exercise1.py
@@ -1,14 +1,25 @@
-name = input("Enter your name")
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-print(f"Hello, {name}!")
+    def display_info(self):
+        return f"Name: {self.name}, Age: {self.age}"
+        class Student(Person):
+            def __init__(self, firstName, lastName, idNumber, scores):
+                super().__init__(firstName, lastName, idNumber)
+                self.scores = scores
 
-
-
-
-def explain_for_loop():
-    for i in range(5):
-        print("Learning Python!")
-    arr = ["apple", "banana", "cherry"]
-    for i in arr :
-        print(i)
-explain_for_loop()
+            def calculate(self):
+                avg = sum(self.scores) / len(self.scores) if self.scores else 0
+                if avg >= 90:
+                    return 'O'
+                if avg >= 80:
+                    return 'E'
+                if avg >= 70:
+                    return 'A'
+                if avg >= 55:
+                    return 'P'
+                if avg >= 40:
+                    return 'D'
+                return 'T'
