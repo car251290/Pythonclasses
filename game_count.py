5828d6f5659c711fd965a9a3e2aa9c2b84f6ebad
Javier  Carreno <35305955+car251290@users.noreply.github.com>
2026-02-11 20:14:48 -0600

    Add files via upload

---
+++ game_count.py
@@ -0,0 +1,23 @@
+def func():
+    print("Hello students!")
+
+    def count_up_to(limit):
+        counter = 1
+
+        while counter <= limit:
+            print(counter)
+            counter += 1
+
+    count_up_to(5)
+
+    def count_down_from(limit):
+        counter = limit
+
+        while counter > 0:
+            print(counter)
+            counter -= 1
+
+    count_down_from(5)
+if __name__ == "__main__":
+    func()
+    
